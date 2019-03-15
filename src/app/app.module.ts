25609182460073aaea62022838c80ecfeb68200b
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-15 12:32:54 -0500

    created component foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IndustriesComponent } from './industries/industries.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { DiscoverComponent } from './discover/discover.component';
+import { CareersComponent } from './careers/careers.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IndustriesComponent,
+    ServicesComponent,
+    ProductsComponent,
+    DiscoverComponent,
+    CareersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
